600c7a31384deeaedb7ddfb5befaf26c0c49a8ac
tinhnd <tinhnd1902@gmail.com>
2023-10-10 15:16:38 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,13 @@ import customConfig from './config/index';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // 作用于全局
-      load: [customConfig], // 加载自定义配置项
+      isGlobal: true,
+      load: [customConfig],
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule], // 数据库配置项依赖于ConfigModule，需在此引入
+      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => configService.get('DATABASE_CONFIG'),
-      inject: [ConfigService], // 记得注入服务，不然useFactory函数中获取不到ConfigService
+      inject: [ConfigService],
     }),
     UserModule
   ],
